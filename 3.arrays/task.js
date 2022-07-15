c978f3b1999ba761674cd878f43b11209762afcf
GuzelMagdeeva <104420578+GuzelMagdeeva@users.noreply.github.com>
2022-07-15 12:20:25 +0300

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = (arr1.length === arr2.length) && (arr1.every((item,idx) => item === arr2[idx]))
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
 
   // Ваш код
 
