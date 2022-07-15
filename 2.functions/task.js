0c5b41bff5fe61e9c32771129aee14358d3b27a1
GuzelMagdeeva <104420578+GuzelMagdeeva@users.noreply.github.com>
2022-07-15 11:56:48 +0300

    Add files via upload

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,52 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  // min и max первый элемент массива
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+ 
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
-  // Ваш код
 
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrOfArr, worker) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++){
+    let arrSum = worker(arrOfArr[i]);
+    if (max<arrSum) {
+      max = arrSum;
+    }
+  }
 
-  // Ваш кода
-  // for ...
-  
+ 
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
+  return getArrayParams(arr).max - getArrayParams(arr).min;
   // Ваш код
 }
